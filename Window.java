45dc97ce1e81db2a2bda8f5d2b022cac5b1605b4
Duckie2369 <100473300+Duckie2369@users.noreply.github.com>
2022-11-10 10:37:45 +0700

    Update Window.java

--- Window.java
+++ Window.java
@@ -1,6 +1,8 @@
 package Project_Plant_Vs_Zombies;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
 
 public class Window extends JFrame {
 	enum Plants{
@@ -16,5 +18,75 @@ public class Window extends JFrame {
 		setSize(1024,768);
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setLayout(null);
+
+		JLabel Sun = new JLabel("SUN");
+		Sun.setLocation(37, 80);
+		Sun.setSize(60, 20);
+
+		Panel gp = new Panel(Sun);
+		gp.setLocation(0, 0);
+		getLayeredPane().add(gp,new valueOf(0));
+		
+		PlaceCard sunflower = new PlaceCard(new ImageIcon(this.getClass().getResource("images/cards/card_sunflower.png")).getImage());
+        sunflower.setLocation(110,8);
+		sunflower.setAction((ActionEvent e) -> {
+			gp.activePlantingBrush = Plants.Sunflower;
+		});
+		getLayeredPane().add(sunflower, 3);
+
+		PlaceCard peashooter = new PlaceCard(new ImageIcon(this.getClass().getResource("images/cards/card_peashooter.png")).getImage());
+        peashooter.setLocation(175,8);
+		peashooter.setAction((ActionEvent e) -> {
+			gp.activePlantingBrush = Plants.Peashooter;
+		});
+		getLayeredPane().add(punflower, 3);
+
+		PlaceCard frozenpeashooter = new PlaceCard(new ImageIcon(this.getClass().getResource("images/cards/card_freezepeashooter.png")).getImage());
+        frozenpeashooter.setLocation(240,8);
+		frozenpeashooter.setAction((ActionEvent e) -> {
+			gp.activePlantingBrush = Plants.FrozenPeashooter;
+		});
+		getLayeredPane().add(frozenpeashooter, 3);
+
+		PlaceCard cherrybomb = new PlaceCard(new ImageIcon(this.getClass().getResource("images/cards/card_cherrybomb.png")).getImage());
+        cherrybomb.setLocation(305,8);
+		cherrybomb.setAction((ActionEvent e) -> {
+			gp.activePlantingBrush = Plants.CherryBomb;
+		});
+		getLayeredPane().add(cherrybomb, 3);
+
+		PlaceCard wallnut = new PlaceCard(new ImageIcon(this.getClass().getResource("images/cards/card_wallnut.png")).getImage());
+        cherrybomb.setLocation(370,8);
+		cherrybomb.setAction((ActionEvent e) -> {
+			gp.activePlantingBrush = Plants.Wallnut;
+		});
+		getLayeredPane().add(wallnut, 3);
+
+
+		getLayeredPane().add(Sun, 2);
+		setResizable(false);
+		setVisible(true);
 	}
+
+	public Window(boolean bool){
+		MenuScreen menu = new MenuScreen();
+		menu.setLocation(0, 0);
+		setSize(1024,768);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setLayeredPane().add(menu, 0);
+		menu.repaint();
+		setResizable(false);
+		setVisible(true);
+	}
+	
+	static Window w;
+	public static void begin(){
+		gw.dispose();
+		gw = new Window();
+	}
+
+
+    public static void main(String[] args) {
+          gw = new Window(true);
+    }
 }
