3a3b6dc61ad45069b2938c8800e1149ac72ff18d
Duckie2369 <100473300+Duckie2369@users.noreply.github.com>
2022-12-02 21:10:17 +0700

    Add files via upload

---
+++ Collisions.java
@@ -0,0 +1,69 @@
+package Project_Plant_Vs_Zombies;
+
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class Collisions extends JPanel implements MouseListener {
+
+    ActionListener al;
+
+    public Collisions(){
+        //setBorder(new LineBorder(Color.RED));
+        setOpaque(false);
+        addMouseListener(this);
+        //setBackground(Color.green);
+        setSize(100,120);
+    }
+
+    public Plant markedPlant;
+
+    public void setPlant(Plant p){
+        markedPlant = p;
+    }
+
+    public void removePlant(){
+        markedPlant.stop();
+        markedPlant = null;
+    }
+
+    public boolean isInsideCollider(int tx){
+        return (tx > getLocation().x) && (tx < getLocation().x + 100);
+    }
+
+    public void setAction(ActionListener al){
+        this.al = al;
+    }
+
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        if(al != null){
+            al.actionPerformed(new ActionEvent(this,ActionEvent.RESERVED_ID_MAX+1,""));
+        }
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
