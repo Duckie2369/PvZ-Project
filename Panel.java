b77ad4516578dde809976d013e5329c1086f011c
Duckie2369 <100473300+Duckie2369@users.noreply.github.com>
2022-12-02 20:55:05 +0700

    Update Panel.java

--- Panel.java
+++ Panel.java
@@ -3,7 +3,14 @@ package Project_Plant_Vs_Zombies;
 import java.awt.Image;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
-
+import java.util.ArrayList;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.*;
 
 public class Panel extends JLayeredPane implements MouseMotionListener{
@@ -12,25 +19,258 @@ public class Panel extends JLayeredPane implements MouseMotionListener{
 	Image peashooter;
 	Image pea;
 	Image frozenPeashooter;
-	Image frozenpea;
+	Image frozenPea;
 	Image wallnut;
 	Image sunflower;
 	Image cherryBomb;
 	
 	Image normalZombie;
-	Image coneHeadZombie;
-	Collision[] = collisions;
+	Image eeHeadZombie;
+	Collision[] collisions;
 	
-	@Override
-	public void mouseDragged(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+	ArrayList<ArrayList<Zombie>> ZombiesLane;
+    ArrayList<ArrayList<Pea>> PeasLane;
+    ArrayList<Sun> activeSuns;
 
-	@Override
-	public void mouseMoved(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	
+    Timer redrawTimer;
+    Timer advancerTimer;
+    Timer sunAppear;
+    Timer zombieAppear;
+    JLabel sunNumber;
+
+    Window.Plants activePlantingBrush = Window.Plants.None;
+
+    int mouseX , mouseY;
+
+    private int sunScore;
+
+    public int getSunScore() {
+        return sunScore;
+    }
+
+    public void setSunScore(int sunScore) {
+        this.sunScore = sunScore;
+        sunScoreboard.setText(String.valueOf(sunScore));
+    }
+
+    public Panel(JLabel sunNumber){
+        setSize(1000,752);
+        setLayout(null);
+        addMouseMotionListener(this);
+        this.sunNumber = sunNumber;
+        setSunScore(150);  //pool avalie
+
+        background  = new ImageIcon(this.getClass().getResource("images/mainBG.png")).getImage();
+
+        peashooter = new ImageIcon(this.getClass().getResource("images/plants/peashooter.gif")).getImage();
+        frozenPeashooter = new ImageIcon(this.getClass().getResource("images/plants/frozenPeashooter.gif")).getImage();
+        sunflower = new ImageIcon(this.getClass().getResource("images/plants/sunflower.gif")).getImage();
+        wallnut = new ImageIcon(this.getClass().getResource("images/plants/wallnut.gif")).getImage();
+        cherryBomb = new ImageIcon(this.getClass().getResource("images/plants/cherry-bomb-powie.gif")).getImage();
+        pea = new ImageIcon(this.getClass().getResource("images/pea.png")).getImage();
+        frozenPea = new ImageIcon(this.getClass().getResource("images/freezepea.png")).getImage();
+
+        normalZombie = new ImageIcon(this.getClass().getResource("images/zombies/zombie1.png")).getImage();
+        coneHeadZombie = new ImageIcon(this.getClass().getResource("images/zombies/zombie2.png")).getImage();
+
+        ZombiesLane = new ArrayList<>();
+        ZombiesLane.add(new ArrayList<>()); //line 1
+        ZombiesLane.add(new ArrayList<>()); //line 2
+        ZombiesLane.add(new ArrayList<>()); //line 3
+        ZombiesLane.add(new ArrayList<>()); //line 4
+        ZombiesLane.add(new ArrayList<>()); //line 5
+
+        PeasLane = new ArrayList<>();
+        PeasLane.add(new ArrayList<>()); //line 1
+        PeasLane.add(new ArrayList<>()); //line 2
+        PeasLane.add(new ArrayList<>()); //line 3
+        PeasLane.add(new ArrayList<>()); //line 4
+        PeasLane.add(new ArrayList<>()); //line 5
+
+        collisions = new Collider[45];
+        for (int i = 0; i < 45; i++) {
+            Collider a = new Collider();
+            a.setLocation(44 + (i%9)*100,109 + (i/9)*120);
+            a.setAction(new PlantActionListener((i%9),(i/9)));
+            collisions[i] = a;
+            add(a,valueOf(0));
+        }
+
+        //collisions[0].setPlant(new frozenPeashooter(this,0,0));
+/*
+        collisions[9].setPlant(new Peashooter(this,0,1));
+        ZombiesLane.get(1).add(new NormalZombie(this,1));*/
+
+        activeSuns = new ArrayList<>();
+
+        redrawTimer = new Timer(25,(ActionEvent e) -> {
+            repaint();
+        });
+        redrawTimer.start();
+
+        advancerTimer = new Timer(60,(ActionEvent e) -> advance());
+        advancerTimer.start();
+
+        sunAppear = new Timer(5000,(ActionEvent e) -> {
+            Random rnd = new Random();
+            Sun sta = new Sun(this,rnd.nextInt(800)+100,0,rnd.nextInt(300)+200);
+            activeSuns.add(sta);
+            add(sta,valueOf(1));
+        });
+        sunAppear.start();
+
+        zombieAppear = new Timer(7000,(ActionEvent e) -> {
+            Random rnd = new Random();
+            DataLevel lvl = new DataLevel();
+            String [] Level = lvl.Level[Integer.parseInt(lvl.lvl)-1];
+            int [][] LevelValue = lvl.LevelValue[Integer.parseInt(lvl.lvl)-1];
+            int l = rnd.nextInt(5);
+            int t = rnd.nextInt(100);
+            Zombie z = null;
+            for(int i = 0;i<LevelValue.length;i++) {
+                if(t>=LevelValue[i][0]&&t<=LevelValue[i][1]) {
+                    z = Zombie.getZombie(Level[i],Panel.this,l);
+                }
+            }
+            ZombiesLane.get(l).add(z);
+        });
+        zombieAppear.start();
+
+    }
+
+    private void advance(){
+        for (int i = 0; i < 5 ; i++) {
+            for(Zombie z : ZombiesLane.get(i)){
+                z.advance();
+            }
+
+            for (int j = 0; j < PeasLane.get(i).size(); j++) {
+                Pea p = PeasLane.get(i).get(j);
+                p.advance();
+            }
+
+        }
+
+        for (int i = 0; i < activeSuns.size() ; i++) {
+            activeSuns.get(i).advance();
+        }
+
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(background,0,0,null);
+
+        //Draw Plants
+        for (int i = 0; i < 45; i++) {
+            Collider c = collisions[i];
+            if(c.assignedPlant != null){
+                Plant p = c.assignedPlant;
+                if(p instanceof Peashooter){
+                    g.drawImage(peashooter,60 + (i%9)*100,129 + (i/9)*120,null);
+                }
+                if(p instanceof frozenPeashooter){
+                    g.drawImage(frozenPeashooter,60 + (i%9)*100,129 + (i/9)*120,null);
+                }
+                if(p instanceof Sunflower){
+                    g.drawImage(sunflower,60 + (i%9)*100,129 + (i/9)*120,null);
+                }
+            }
+        }
+
+        for (int i = 0; i < 5 ; i++) {
+            for(Zombie z : ZombiesLane.get(i)){
+                if(z instanceof NormalZombie){
+                    g.drawImage(normalZombie,z.posX,109+(i*120),null);
+                }else if(z instanceof ConeHeadZombie){
+                    g.drawImage(coneHeadZombie,z.posX,109+(i*120),null);
+                }
+            }
+
+            for (int j = 0; j < PeasLane.get(i).size(); j++) {
+                Pea p = PeasLane.get(i).get(j);
+                if(p instanceof FreezePea){
+                    g.drawImage(frozenPea, p.posX, 130 + (i * 120), null);
+                }else {
+                    g.drawImage(pea, p.posX, 130 + (i * 120), null);
+                }
+            }
+
+        }
+
+        //if(!"".equals(activePlantingBrush)){
+            //System.out.println(activePlantingBrush);
+            /*if(activePlantingBrush == Window.Plants.Sunflower) {
+                g.drawImage(sunflower,mouseX,mouseY,null);
+            }*/
+
+        //}
+
+
+    }
+
+    class PlantActionListener implements ActionListener {
+
+        int x,y;
+
+        public PlantActionListener(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(activePlantingBrush == Window.Plants.Sunflower){
+                if(getSunScore()>=50) {
+                    collisions[x + y * 9].setPlant(new Sunflower(Panel.this, x, y));
+                    setSunScore(getSunScore()-50);
+                }
+            }
+            if(activePlantingBrush == Window.Plants.Peashooter){
+                if(getSunScore() >= 100) {
+                    collisions[x + y * 9].setPlant(new Peashooter(Panel.this, x, y));
+                    setSunScore(getSunScore()-100);
+                }
+            }
+
+            if(activePlantingBrush == Window.Plants.FrozenPeashooter){
+                if(getSunScore() >= 175) {
+                    collisions[x + y * 9].setPlant(new frozenPeashooter(Panel.this, x, y));
+                    setSunScore(getSunScore()-175);
+                }
+            }
+            activePlantingBrush = Window.Plants.None;
+        }
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        mouseX = e.getX();
+        mouseY = e.getY();
+    }
+    static int progress = 0;
+    public static void setProgress(int num) {
+        progress = progress + num;
+        System.out.println(progress);
+        if(progress>=150) {
+           if("1".equals(DataLevel.Lvl)) {
+            JOptionPane.showMessageDialog(null,"Level Completed !!!" + '\n' + "Starting next Level");
+            Window.w.dispose();
+            DataLevel.write("2");
+            Window.w = new Window();
+            }  else {
+               JOptionPane.showMessageDialog(null,"Level Completed !!!" + '\n' + "More Levels will come soon !!!" + '\n' + "Resetting data");
+               DataLevel.write("1");
+               System.exit(0);
+           }
+           progress = 0;
+        }
+    }
 }
+
